51c8da2e06e4748cafbf838e751e0d81a22000e8
Jasen <60624556+jasenscode@users.noreply.github.com>
2021-11-15 20:16:38 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+// "use strict";
+
+// const bomb = document.querySelector("#bomb");
+// const frog = document.querySelectorAll(".frog");
+// const topLane = document.querySelector("#top-lane");
+// const bottomLane = document.querySelector("#bottom-lane");
+// const bombPosition = bomb.getBoundingClientRect();
+// // const frogPosition = frog.getBoundingClientRect();
+// const jumpBtn = document.querySelector("#jump-btn");
+
+// // if (bombPosition.right >= 292) {
+// //   alert("it has a frog!");
+// // }
+
+// // const checkBombPosition = setInterval(() => {
+// //   console.log(bombPosition.right);
+// //   console.log(bombPosition.left);
+// // }, 500);
+
+// // bottomLane.contains(frog) &&
+
+// // bomb.addEventListener("animationend", () => {
+// //   console.log(bombPosition.left);
+// // });
+
+// // make jump button move frog between divs
+// // need to add forEach
+
+// // jumpBtn.addEventListener("click", () => {
+// //   frog.forEach((frog) => {
+// //     if (frog.style.display === "block") {
+// //       frog.style.display = "none";
+// //     } else {
+// //       frog.style.display = "block";
+// //     }
+// //   });
+// // });
+
+// const bombPos = document.querySelector("#bomb");
+// // console.log(bombPos.offsetLeft, bombPos.offsetTop);
+
+// // USE THIS TO CHECK POSITION OF BOMB
+// // const checkBombPosition = setInterval(() => {
+// //   console.log(bombPos.offsetLeft);
+// // }, 250);
