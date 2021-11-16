dfd76a6cd571faf38a92902f8df42da673a7d91f
Jasen <60624556+jasenscode@users.noreply.github.com>
2021-11-16 16:47:16 +0000

    Added collision detection

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // Selectors
 const bomb = document.querySelector("#bomb");
-const character = document.querySelector(".character");
+const character = document.querySelector("#character");
 const jumpBtn = document.querySelector("#jump-btn");
 const startBtn = document.querySelector("#start-btn");
 const resetBtn = document.querySelector("#reset-btn");
@@ -14,7 +14,7 @@ const highScore = document.querySelector("#high-score");
 // function to start game
 const handleStart = () => {
   bomb.classList.add("animate-bomb");
-  currentScore.innerHTML = "0";
+  currentScore.innerHTML = 0;
 };
 
 startBtn.addEventListener("click", handleStart);
@@ -30,49 +30,34 @@ const handleJump = () => {
 jumpBtn.addEventListener("click", handleJump);
 
 // function to calculate position of bomb
+const getPositions = setInterval(() => {
+  if (character.offsetTop >= 112 && bomb.offsetLeft <= 50) {
+    alert("u lose");
+  }
+  //   console.log(bomb.offsetLeft, bomb.offsetTop);
+  //   console.log(character.offsetLeft, character.offsetTop);
+}, 10);
 
 // function to check if bomb has reached end of animation without hitting character
-//// adds one to score
-//// if it does collide then score is added to high score if it is higher
+
+const setCurrentScore = () => {
+  currentScore.innerHTML++;
+};
+
+bomb.addEventListener("animationiteration", setCurrentScore);
 
 // function to reset the game - HANDLE RESET
 const handleReset = () => {
   bomb.classList.remove("animate-bomb");
-  currentScore.innerHTML = "0";
-  highScore.innerHTML = "0";
+  currentScore.innerHTML = 0;
+  highScore.innerHTML = 0;
 };
 
 resetBtn.addEventListener("click", handleReset);
-//// removes animation class from bomb
-//// reset score to zero
-//// reset high score to zero
 
 // // if (bombPosition.right >= 22) {
 // //   alert("it has a frog!");
 // // }
-
-// const checkBombPosition = setInterval(() => {
-//   console.log(bombPosition.right);
-//   console.log(bombPosition.left);
-// }, 500);
-
-// // bottomLane.contains(frog) &&
-
-// // bomb.addEventListener("animationend", () => {
-// //   console.log(bombPosition.left);
-// // });
-
-// const bombPos = document.querySelector("#bomb");
-// // // console.log(bombPos.offsetLeft, bombPos.offsetTop);
-
-// USE THIS TO CHECK POSITION OF BOMB
-// const checkBombPosition = setInterval(() => {
-//   console.log(bombPos.offsetLeft);
-// }, 50);
-
-// console.log(typeof bombPos.offsetLeft);
-// console.log(jumpBtn);
-// console.log(character);
 
 // Add year for footer
 const getYear = new Date();
